bdcb69508a159647d4152d08819ba3c0ed9453a3
Darshan Patel <darshan.patel@kiprosh.com>
2021-08-04 00:17:05 +0530

    first commit

---
+++ My Quiz/My Quiz/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  My Quiz
+//
+//  Created by Darshan on 03/08/21.
+//
+
+import SwiftUI
+import FirebaseFirestoreSwift
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
